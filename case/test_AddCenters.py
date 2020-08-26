de12ed75c441e0576f67f130aa87685ce075cd6c
likedi421847541 <421847541@qq.com>
2020-08-26 11:38:50 +0800

    添加学校增加后置条件：删除学校（避免数据量大）

--- case/test_AddCenters.py
+++ case/test_AddCenters.py
@@ -12,6 +12,9 @@ class AddCenters_001(unittest.TestCase):
     def setUpClass(cls):
         cls.token = get_token()
         cls.user_id = get_userid()
+    def setUp(self):
+        self.s = requests.session()
+        self.g = globals()
     def test_addcenters(self):
         center_name = random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()')
         url = 'http://api2.learning-genie-api.com/api/v1/centers'
@@ -21,8 +24,16 @@ class AddCenters_001(unittest.TestCase):
                    'User-Agent':'Mozilla/5.0 (Windows NT 10.0;   WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
                    'X-LG-Token':self.token,
                    'X-UID':self.user_id}
-        login = demo.RunMain().run_main('post',url,data=data,headers=headers)
-        login = json.loads(login)
-        return login['id']
+        centers = demo.RunMain().run_main('post',url,data=data,headers=headers)
+        centers_id = json.loads(centers)['id']
+        self.g['id'] = centers_id
+    def tearDown(self):
+        url = 'http://api2.learning-genie-api.com/api/v1/centers/'+self.g['id']
+        #print (url)
+        headers = {'Content-Type': 'application/json;charset=UTF-8',
+                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;   WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+                   'X-LG-Token': self.token,
+                   'X-UID': self.user_id}
+        del_centers = requests.delete(url,headers = headers)
     if __name__ == '__main__':
         test_addcenters()
